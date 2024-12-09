ee51fe16abab6c61e6dcd85a87f0cefe95c6668f
dspuliaiev <d.s.puliaiev@gmail.com>
2024-12-09 11:29:21 +0200

    Red_conf

--- backend/settings.py
+++ backend/settings.py
@@ -92,7 +92,7 @@ CHANNEL_LAYERS = {
         'CONFIG': {
             "hosts": [{
                 "host": os.getenv('REDIS_HOST'),
-                "port": int(os.getenv('REDIS_PORT')),
+                "port": os.getenv('REDIS_PORT'),
                 "password": os.getenv('REDIS_PASSWORD'),
             }],
         },
